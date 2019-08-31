d16032d582c5e4f55b88d9cc35cdd68d495c20fe
bleauwonder <elyzableau.om@gmail.com>
2019-08-31 12:41:50 -0400

    Updates

---
+++ precache-manifest.d00f92a1c06026e854b1c99b95e448b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590aecea5d19627a095fa9691265ada7",
+    "url": "/matching-cities-game/index.html"
+  },
+  {
+    "revision": "95a2e85ec2e3cbc309fe",
+    "url": "/matching-cities-game/static/css/main.6d4d8cee.chunk.css"
+  },
+  {
+    "revision": "5eaa8df0403fae0c715b",
+    "url": "/matching-cities-game/static/js/2.1e371dae.chunk.js"
+  },
+  {
+    "revision": "95a2e85ec2e3cbc309fe",
+    "url": "/matching-cities-game/static/js/main.7e012828.chunk.js"
+  },
+  {
+    "revision": "243881458cafc9822092",
+    "url": "/matching-cities-game/static/js/runtime~main.44506b05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8af4da27b9fc0cb5c802915700c1cd31.js"
+  "/matching-cities-game/precache-manifest.d00f92a1c06026e854b1c99b95e448b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/matching-cities-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
